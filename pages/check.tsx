faf2fb13574e8772aa10014c722a570f7917908a
Boonsuen Oh <boonsuenoh@gmail.com>
2023-05-07 14:53:27 +0800

    Check page UI

--- pages/check.tsx
+++ pages/check.tsx
@@ -1,6 +1,14 @@
 import Layout, { pages } from '@/components/layout/Layout';
+import { useState } from 'react';
 
 export default function CheckDurianPage() {
+  const [durianId, setDurianId] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('Form submission data', { durianId });
+  };
+
   return (
     // TODO: Current role will be dynamically determined and passed in
     <Layout currentPage="/check" currentRole="guest">
@@ -9,7 +17,7 @@ export default function CheckDurianPage() {
           {pages['/check'].title}
         </h1>
         <div>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="grid gap-6 mb-6 md:grid-cols-2">
               <div>
                 <label
@@ -20,6 +28,8 @@ export default function CheckDurianPage() {
                 </label>
                 <input
                   type="text"
+                  value={durianId}
+                  onChange={(e) => setDurianId(e.target.value)}
                   id="durian-id"
                   className="relative transition-all duration-300 py-2.5 pl-4 pr-14 w-full border-gray-300 dark:bg-slate-800 dark:text-white/80 dark:border-slate-600 rounded-lg tracking-wide font-light text-sm placeholder-gray-400 bg-white focus:ring disabled:opacity-40 disabled:cursor-not-allowed focus:border-green-500 focus:ring-green-500/20"
                   placeholder="123"
