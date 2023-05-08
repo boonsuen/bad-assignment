f4e16b41aba0f973a350efd5d90d48a710104547
Boonsuen Oh <boonsuenoh@gmail.com>
2023-05-09 00:04:17 +0800

    Add Consumer auth

--- pages/add-consumer.tsx
+++ pages/add-consumer.tsx
@@ -5,7 +5,6 @@ import { useContext, useEffect, useState } from 'react';
 
 export default function AddConsumerPage() {
   // ---------------------------------------------------------------------//
-  const [isLoading, setIsLoading] = useState(true);
   const {
     currentAccount,
     checkIfWalletIsConnected,
@@ -14,30 +13,23 @@ export default function AddConsumerPage() {
   } = useContext(DTraceContext);
   const [role, setRole] = useState<roles | null>(null);
 
-  const router = useRouter();
-
   useEffect(() => {
-    checkIfWalletIsConnected().then(async (res) => {
-      if (!res) {
-        router.push('/');
-      }
-
-      setIsLoading(false);
-    });
+    checkIfWalletIsConnected();
 
     if (currentAccount) {
       console.log('currentAccount', currentAccount);
 
       checkAccountType(currentAccount).then((accountType) => {
-        console.log(accountType);
-
         setRole(accountType as roles);
       });
+    } else {
+      setRole(null);
     }
   }, [currentAccount]);
-  console.log('role', role);
 
+  console.log('role', role);
   // ---------------------------------------------------------------------//
+
   const [consumerAccountAddress, setConsumerAccountAddress] = useState('');
   const [consumerName, setConsumerName] = useState('');
 
@@ -46,93 +38,69 @@ export default function AddConsumerPage() {
     addConsumer(consumerAccountAddress, consumerName);
   };
 
-  if (isLoading) {
-    return (
-      <div className="flex justify-center items-center w-screen h-screen">
-        <div role="status">
-          <svg
-            aria-hidden="true"
-            className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              fill="currentColor"
-            />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentFill"
-            />
-          </svg>
-          <span className="sr-only">Loading...</span>
+  return (
+    <Layout
+      currentPage="/add-consumer"
+      currentRole={
+        role !== null
+          ? ((role.toLowerCase() === 'owneroradmin'
+              ? 'admin'
+              : role.toLowerCase()) as roles)
+          : 'guest'
+      }
+    >
+      <div className="p-4 md:ml-64">
+        <h1 className="text-2xl font-semibold text-slate-800 mt-3 mb-5">
+          {pages['/add-consumer'].title}
+        </h1>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <div className="grid gap-6 mb-6 md:grid-cols-2">
+              <div className="row-start-1">
+                <label
+                  htmlFor="consumer-account-address"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Consumer Account Address
+                </label>
+                <input
+                  type="text"
+                  id="consumer-account-address"
+                  value={consumerAccountAddress}
+                  onChange={(e) => setConsumerAccountAddress(e.target.value)}
+                  className="relative transition-all duration-300 py-2.5 px-4 w-full border-gray-300 dark:bg-slate-800 dark:text-white/80 dark:border-slate-600 rounded-lg tracking-wide font-light text-sm placeholder-gray-400 bg-white focus:ring disabled:opacity-40 disabled:cursor-not-allowed focus:border-green-500 focus:ring-green-500/20"
+                  placeholder="e.g. 0x71C7656EC7ab88b098defB751B7401B5f6d8976F"
+                  required
+                />
+              </div>
+
+              <div className="row-start-2">
+                <label
+                  htmlFor="consumer-name"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Consumer Name
+                </label>
+                <input
+                  type="text"
+                  id="consumer-name"
+                  value={consumerName}
+                  onChange={(e) => setConsumerName(e.target.value)}
+                  className="relative transition-all duration-300 py-2.5 px-4 w-full border-gray-300 dark:bg-slate-800 dark:text-white/80 dark:border-slate-600 rounded-lg tracking-wide font-light text-sm placeholder-gray-400 bg-white focus:ring disabled:opacity-40 disabled:cursor-not-allowed focus:border-green-500 focus:ring-green-500/20"
+                  placeholder="e.g. Wilson Chai Boon Xiang"
+                  required
+                />
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="text-white rounded-lg bg-primary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
+          </form>
         </div>
       </div>
-    );
-  }
-
-  if (currentAccount && role !== null) {
-    return (
-      // TODO: Current role will be dynamically determined and passed in
-      <Layout currentPage="/add-consumer" currentRole={
-        (role.toLowerCase() === 'owneroradmin'
-          ? 'admin'
-          : role.toLowerCase()) as roles
-      }>
-        <div className="p-4 md:ml-64">
-          <h1 className="text-2xl font-semibold text-slate-800 mt-3 mb-5">
-            {pages['/add-consumer'].title}
-          </h1>
-          <div>
-            <form onSubmit={handleSubmit}>
-              <div className="grid gap-6 mb-6 md:grid-cols-2">
-                <div className="row-start-1">
-                  <label
-                    htmlFor="consumer-account-address"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Consumer Account Address
-                  </label>
-                  <input
-                    type="text"
-                    id="consumer-account-address"
-                    value={consumerAccountAddress}
-                    onChange={(e) => setConsumerAccountAddress(e.target.value)}
-                    className="relative transition-all duration-300 py-2.5 px-4 w-full border-gray-300 dark:bg-slate-800 dark:text-white/80 dark:border-slate-600 rounded-lg tracking-wide font-light text-sm placeholder-gray-400 bg-white focus:ring disabled:opacity-40 disabled:cursor-not-allowed focus:border-green-500 focus:ring-green-500/20"
-                    placeholder="e.g. 0x71C7656EC7ab88b098defB751B7401B5f6d8976F"
-                    required
-                  />
-                </div>
-
-                <div className="row-start-2">
-                  <label
-                    htmlFor="consumer-name"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Consumer Name
-                  </label>
-                  <input
-                    type="text"
-                    id="consumer-name"
-                    value={consumerName}
-                    onChange={(e) => setConsumerName(e.target.value)}
-                    className="relative transition-all duration-300 py-2.5 px-4 w-full border-gray-300 dark:bg-slate-800 dark:text-white/80 dark:border-slate-600 rounded-lg tracking-wide font-light text-sm placeholder-gray-400 bg-white focus:ring disabled:opacity-40 disabled:cursor-not-allowed focus:border-green-500 focus:ring-green-500/20"
-                    placeholder="e.g. Wilson Chai Boon Xiang"
-                    required
-                  />
-                </div>
-              </div>
-              <button
-                type="submit"
-                className="text-white rounded-lg bg-primary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:focus:ring-blue-800"
-              >
-                Submit
-              </button>
-            </form>
-          </div>
-        </div>
-      </Layout>
-    );
-  }
+    </Layout>
+  );
 }
