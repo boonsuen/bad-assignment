8dae5ba48b4913fd66f5c59c1a64011c62252c14
Wickey Chai <wickeychai02@gmail.com>
2023-05-08 23:14:23 +0800

    usecontext

--- pages/add-durian.tsx
+++ pages/add-durian.tsx
@@ -1,14 +1,47 @@
-import Layout, { pages } from '@/components/layout/Layout';
-import { DTraceContext } from '@/context/Dtrace';
+import Layout, { pages, roles } from '@/components/layout/Layout';
+import { DTraceContext } from '../context/Dtrace';
+import { useRouter } from 'next/router';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { Rating } from '@/types';
-import { useCallback, useContext, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import Datepicker from 'react-tailwindcss-datepicker';
+import { getUnixTime } from 'date-fns';
 
 export default function AddDurianPage() {
+  // ---------------------------------------------------------------------//
+  const [isLoading, setIsLoading] = useState(true);
+  const { currentAccount, checkIfWalletIsConnected, checkAccountType, addDurian } =
+    useContext(DTraceContext);
+  const [role, setRole] = useState<roles | null>(null);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    checkIfWalletIsConnected().then(async (res) => {
+      if (!res) {
+        router.push('/');
+      }
+
+      setIsLoading(false);
+    });
+
+    if (currentAccount) {
+      console.log('currentAccount', currentAccount);
+
+      checkAccountType(currentAccount).then((accountType) => {
+        console.log(accountType);
+
+        setRole(accountType as roles);
+      });
+    }
+  }, [currentAccount]);
+  console.log('role', role);
+
+  // ---------------------------------------------------------------------//
+
   const [varietyCode, setVarietyCode] = useState('');
-  const [farmId, setFarmId] = useState('');
-  const [treeId, setTreeId] = useState('');
+  const [farmId, setFarmId] = useState<number>();
+  const [treeId, setTreeId] = useState<number>();
   const [condition, setCondition] = useState<Rating>('Excellent');
   const [harvestedDate, setHarvestedDate] = useState({
     startDate: new Date(),
@@ -59,8 +92,77 @@ export default function AddDurianPage() {
       harvestedTime,
       fileUrl,
     });
+
+    const combinedDate = new Date(
+      harvestedDate.startDate.getFullYear(),
+      harvestedDate.startDate.getMonth(),
+      harvestedDate.startDate.getDate(),
+      parseInt(harvestedTime.split(':')[0], 10),
+      parseInt(harvestedTime.split(':')[1], 10)
+    );
+
+    const unixHarvestedTime = getUnixTime(combinedDate);
+
+    let conditionFarm:number; 
+
+    switch(condition){
+      case 'Excellent':
+        conditionFarm = 4;
+        break;
+      case 'Good':
+        conditionFarm = 3;
+        break;
+      case 'Fair':
+        conditionFarm = 2;
+        break;
+      case 'Poor':
+        conditionFarm = 1;
+        break;
+      case 'Bad':
+        conditionFarm = 0;
+        break;
+      default:
+        conditionFarm = 0;
+        break;
+    }
+
+    addDurian(
+      farmId as number,
+      treeId as number,
+      varietyCode,
+      unixHarvestedTime,
+      fileUrl,
+      conditionFarm
+    )
   };
 
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center w-screen h-screen">
+        <div role="status">
+          <svg
+            aria-hidden="true"
+            className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    );
+  }
+
+  if (currentAccount && role !== null) {
   return (
     // TODO: Current role will be dynamically determined and passed in
     <Layout currentPage="/add-durian" currentRole="farmer">
@@ -97,10 +199,10 @@ export default function AddDurianPage() {
                   Farm ID
                 </label>
                 <input
-                  type="text"
+                  type="number"
                   id="farm-id"
                   value={farmId}
-                  onChange={(e) => setFarmId(e.target.value)}
+                  onChange={(e) => setFarmId(parseInt(e.target.value))}
                   className="relative transition-all duration-300 py-2.5 pl-4 pr-14 w-full border-gray-300 dark:bg-slate-800 dark:text-white/80 dark:border-slate-600 rounded-lg tracking-wide font-light text-sm placeholder-gray-400 bg-white focus:ring disabled:opacity-40 disabled:cursor-not-allowed focus:border-green-500 focus:ring-green-500/20"
                   placeholder="e.g. 1"
                   required
@@ -118,7 +220,7 @@ export default function AddDurianPage() {
                   type="text"
                   id="tree-id"
                   value={treeId}
-                  onChange={(e) => setTreeId(e.target.value)}
+                  onChange={(e) => setTreeId(parseInt(e.target.value))}
                   className="relative transition-all duration-300 py-2.5 pl-4 pr-14 w-full border-gray-300 dark:bg-slate-800 dark:text-white/80 dark:border-slate-600 rounded-lg tracking-wide font-light text-sm placeholder-gray-400 bg-white focus:ring disabled:opacity-40 disabled:cursor-not-allowed focus:border-green-500 focus:ring-green-500/20"
                   placeholder="e.g. 1"
                   required
@@ -243,4 +345,5 @@ export default function AddDurianPage() {
       </div>
     </Layout>
   );
+}
 }
